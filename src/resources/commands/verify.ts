0048247977f1d0ef962eb68173295ed2d4b55454
SK-Fast <thummajil25580@gmail.com>
2022-01-25 08:50:01 +0700

    devpixels so dumb omg

--- src/resources/commands/verify.ts
+++ src/resources/commands/verify.ts
@@ -16,21 +16,20 @@ export const description = 'Get your Discord account verified!'
   * Command main function
   */
 export const main = async function (message: Message, args: string[]) {
-  // @ts-expect-error
-  const verifiedRoleConfig = await firebaseUtils.getSpecificServerConfig(message.guild.id, 'verifiedRole')
-  if (verifiedRoleConfig) {
-    // @ts-expect-error
-    const role = message.guild.roles.cache.find(r => r.id === verifiedRoleConfig)
-
-    // @ts-expect-error
-    message.member.roles.add(role)
-
-    message.channel.send('Successfully gave you verified role!')
-    return
-  }
-
   const isVerified = await firebaseUtils.isVerified(message.author.id)
   if (isVerified === true) {
+    // @ts-expect-error
+    const verifiedRoleConfig = await firebaseUtils.getSpecificServerConfig(message.guild.id, 'verifiedRole')
+    if (verifiedRoleConfig) {
+      // @ts-expect-error
+      const role = message.guild.roles.cache.find(r => r.id === verifiedRoleConfig)
+
+      // @ts-expect-error
+      message.member.roles.add(role)
+
+      message.channel.send('Successfully gave you verified role!')
+      return
+    }
     message.channel.send('Your Polytoria account has already been verified. To unlink use `!poly unverify`')
     return
   }
